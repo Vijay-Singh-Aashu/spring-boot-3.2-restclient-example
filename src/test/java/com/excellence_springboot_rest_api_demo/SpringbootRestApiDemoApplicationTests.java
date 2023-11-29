2c4860f9d3c08705aabccada37b29dc4d6539574
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-29 10:46:21 +0530

    base-code-created

---
+++ src/test/java/com/excellence_springboot_rest_api_demo/SpringbootRestApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence_springboot_rest_api_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
